1911edd7634011d048280eb108cda100c66b04bf
bartoszwrobel <bartoszwrobel591@gmail.com>
2022-07-05 15:52:47 +0200

    Updated CORS rules

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ AppDataSource.initialize()
     app.use(bodyParser.json());
 
     app.use(function (req, res, next) {
-      res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+      res.header("Access-Control-Allow-Origin", "*");
       res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
