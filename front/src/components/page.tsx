8eb47151a23c650995584157407834a6a1af5778
Rafał Sztwiorok <rafal.sztwiorok@gmail.com>
2022-07-20 14:40:26 +0200

    Update page.tsx

--- front/src/components/page.tsx
+++ front/src/components/page.tsx
@@ -6,7 +6,7 @@ interface IPageProps {
 
 const Page = ({ children }: IPageProps) => {
   return (
-    <div className="flex justify-center align-middle h-screen bg-emerald-50">
+    <div className="flex justify-center align-middle h-screen bg-red-500">
       {children}
     </div>
   );
